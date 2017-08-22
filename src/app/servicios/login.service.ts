916b112ee900ddf148e09970afcbd764692e69a9
root <root@localhost.localdomain>
2017-08-21 22:59:28 -0400

    Login daw

---
+++ src/app/servicios/login.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/Rx'
+
+@Injectable()
+export class LoginService {
+  url:string="https://optimux-a0924.firebaseio.com/usuarios.json"
+
+  constructor(private http:Http) { }
+
+  logear(datos){
+    this.getUsuario().subscribe(usu=>{
+      
+    })
+  }
+
+
+  getUsuario(){
+    return this.http.get(this.url).map(usu=>{
+      return usu.json()
+    })
+  }
+
+}
