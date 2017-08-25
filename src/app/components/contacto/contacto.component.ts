c8fcc168fbed59c126f44ba8d7f71cb3830d318d
root <root@localhost.localdomain>
2017-08-25 06:07:22 -0400

    Contactos terminados

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+declare var $
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactoComponent implements OnInit {
 
+contac={
+  from_name:"",
+  numero:"",
+  asunto:"",
+  message_html:"",
+  email_address:""
+}
+validar:FormGroup;
+
   constructor() { }
 
+
+
+
+Contacto(){
+  setTimeout(this.Actualizar(),10000)
+}
+
+  Actualizar(){
+    alert("Se ha enviado correctamente")
+    $('.datos').val('')
+
+  }
+
   ngOnInit() {
+    this.validar=new FormGroup({
+      'from_name':new FormControl('',[Validators.required,Validators.pattern("[a-zA-ZñÑáéíóúÁÉÍÓÚüÜ]{1,}")]),
+      'numero':new FormControl('',[Validators.required,Validators.pattern("[0-9]{1,}")]),
+      'asunto':new FormControl('',[Validators.required]),
+      'message_html':new FormControl('',[Validators.required]),
+      'email_address':new FormControl('',[Validators.required,Validators.pattern("[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}")])
+
+    })
   }
 
 }
