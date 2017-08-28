f9a01089d72073fda919753ff6b036783ef4b655
root <root@localhost.localdomain>
2017-08-28 04:00:31 -0400

    boton de ofertas

--- src/app/components/inicio/ofert/ofert.component.ts
+++ src/app/components/inicio/ofert/ofert.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {VehiculosService} from '../../../servicios/vehiculos.service'
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-ofert',
@@ -7,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfertComponent implements OnInit {
 
-  constructor() { }
+estadoOfertas:boolean=false
+  queryObservable:FirebaseListObservable<any>
+  constructor(private db:AngularFireDatabase) {
+
+    const queryObservable = db.list('/vehiculos', {
+  query: {
+    orderByChild: 'estado',
+    equalTo: 2
+  }
+}).subscribe(gg=>{
+  if(gg.length>0){
+    this.estadoOfertas=true
+  }
+});
+   }
 
   ngOnInit() {
   }
