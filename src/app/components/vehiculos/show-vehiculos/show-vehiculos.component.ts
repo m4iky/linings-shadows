f8921afa371b161c45e518ed98ea55482cfb06fd
Bryan <user990624@gmail.com>
2017-08-28 08:55:53 -0600

    Paginacion

--- src/app/components/vehiculos/show-vehiculos/show-vehiculos.component.ts
+++ src/app/components/vehiculos/show-vehiculos/show-vehiculos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {VehiculosService} from './../../../servicios/vehiculos.service';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import {PaginacionService} from './../../../servicios/paginacion.service';
 declare var $
 @Component({
   selector: 'app-show-vehiculos',
@@ -7,20 +9,44 @@ declare var $
   styleUrls: ['./show-vehiculos.component.css']
 })
 export class ShowVehiculosComponent implements OnInit {
+//propiedades para paginacion
+asincronico:FirebaseListObservable<any[]>
+private allItems: any[];
+pager:any = {}
+pagedItems: any[];
+//fin propiedades
 
-  constructor(private _mostrarVehiculos:VehiculosService) {
+
+  constructor(private _mostrarVehiculos:VehiculosService,private db:AngularFireDatabase,private paginacionService:PaginacionService) {
     $(document).ready(function(){
 $('.tooltipped').tooltip({delay: 50});
 });
 
    }
-
-
   ngOnInit() {
+    //paginacion
+    this.asincronico = this.db.list('vehiculos');
+    this.asincronico.subscribe(res =>{
+      this.allItems = res
+      this.setPage(1)
+    })
+    //fin paginacion
     this._mostrarVehiculos.mostrarVehiculos()
     $(document).ready(function(){
    $('.tooltipped').tooltip({delay: 50});
  });
   }
+
+  setPage(page: number) {
+        if (page < 1 || page > this.pager.totalPages) {
+            return;
+        }
+
+        // get pager object from service
+        this.pager = this.paginacionService.getPager(this.allItems.length, page);
+
+        // get current page of items
+        this.pagedItems = this.allItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
+    }
 
 }
